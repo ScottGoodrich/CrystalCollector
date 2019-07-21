ea5f91a27d7a4fd1919a476a3150fda9f64d9f25
Scott Goodrich <scottgoodrich516@gmail.com>
2019-07-20 23:08:05 -0700

    complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,19 +9,22 @@ console.log(targetNumber);
 $(document).ready(function () {
 
     function newGame() {
+        score = 0;
+        randomNumber();
         $(".score-text").empty();
         $(".target-text").html(targetNumber);
         $(".score-text").html(score);
     }
 
-    /*function randomNumber() {
+    function randomNumber() {
         for (var i = 0; i < 4; i++) {
             var randNum = Math.floor(Math.random() * 12) + 1;
             console.log(randNum);
             var crystal = $("#crystal" + (i + 1));
             crystal.attr("data-value", randNum);
         }
-    } */
+        targetNumber = Math.floor(Math.random() * 102) + 19;
+    } 
     for (var i = 0; i < 4; i++) {
         var randNum = Math.floor(Math.random() * 12) + 1;
         console.log(randNum);
